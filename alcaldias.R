19620cdb0e8f8125eb30eb38b6438dfaa2b6d9ed
ivanhigueram <ivanhigueram@gmail.com>
2016-12-13 08:44:05 -0500

    Clean mayor data :ok_hand:

--- alcaldias.R
+++ alcaldias.R
@@ -28,65 +28,76 @@ list_files <- list.files() %>%
   str_c("~/Dropbox/BANREP/Elecciones/Data/CEDE/Microdatos", c("1997", "2000", "2003", "2007", "2011", "2015"),
         ., sep = "/")
 
+
+#Open dta files into a list
+alcaldes <- lapply(list_files, read_dta)
+
+
+#Aggregate totals for each year and clean non-candidate data
+
 non_candidate_votes <- c("VOTOS EN BLANCO", "VOTOS NULOS", "TARJETAS NO MARCADAS",
                          "Votos en blanco", "Votos nulos", "Tarjetas no marcadas",
                          "Votos no marcados")
 
-alcaldes <- lapply(list_files, read_dta)
-
-
-alcaldes_merge <- alcaldes %>%
+alcaldes_aggregate <- alcaldes %>%
   lapply(., function(x){
     arrange(x, codmpio, ano) %>%
-    mutate(non_candidate = ifelse(primer_apellido %in% non_candidate_votes, 1, 0)) %>%
+    mutate(non_candidate = ifelse(primer_apellido %in% non_candidate_votes, 1, 0)) %>% 
     group_by(codmpio, ano) %>%
     mutate(prop_votes_total = votos / sum(votos)) %>%
     filter(non_candidate == 0) %>%
     mutate(prop_votes_candidates = votos / sum(votos)) %>%
     mutate(rank = dense_rank(desc(votos))) %>%
-    filter(rank <= 2)
+    filter(rank <= 2) %>%
+    filter(prop_votes_candidates < 1) #Eliminate elections with only the winner reported
   })
 
-
+#Arrange data in a long format
+alcaldes_merge <- alcaldes_aggregate %>%
+  ldply() %>%
+  arrange(codmpio, ano, desc(rank)) %>%
+  dplyr::select(c(ano, codmpio, codep, municipio, departamento, primer_apellido, nombre, codpartido, votos, 
+                  prop_votes_total, prop_votes_candidates, rank))
 
 ###########################################################################################################
 ############################################### ARRANGE DATA ##############################################
 ###########################################################################################################
 
-setwd("~/Google Drive/DATOS_ELECTORALES/CEDE/Archivos_DTA/")
-alcaldias <- read_dta("Alcaldias.dta") %>%
-  arrange(muni_code, year, desc(posici_muni)) %>%
-  filter(year >= 1997) %>%
-  dplyr::group_by(muni_code, year) %>% #Calculate difference
-  dplyr::mutate(diff =  ave(pvotos_muni, factor(muni_code), factor(year), FUN = function(x) c(0, diff(x)))) 
+alcaldes_difference <- alcaldes_merge %>%
+  arrange(codmpio, ano, desc(rank)) %>%
+  group_by(codmpio, ano) %>% #Calculate difference
+  mutate(diff =  ave(prop_votes_total, factor(codmpio), factor(ano), FUN = function(x) c(0, diff(x)))) 
   # %>%
   # dplyr::mutate(diff = ifelse(posici_muni == 1 & pvotos_muni_can == 100 & diff == 0, 100, diff))
 
 #Collapse data.frame
 
-alcaldias <- alcaldias %>%
-  dplyr::group_by(muni_code, year) %>%
+alcaldes_difference <- alcaldes_difference %>%
+  dplyr::group_by(codmpio, ano) %>%
   dplyr::summarize(difference = sum(diff))
 
-alcaldias_wide <- alcaldias %>%
-  spread(year, difference, fill = 100, sep = "") 
+alcaldes_wide <- alcaldes_difference %>%
+  spread(ano, difference, sep = "") 
 
-alcaldias_long <- alcaldias_wide %>%
-  gather(year, diff, year1997:year2015)
+#Remove obs with at least one NA
+alcades_wide <- head(alcaldes_wide[complete.cases(alcaldes_wide), ])
+
+alcaldes_long <- alcaldes_wide %>%
+  gather(ano, diff, ano1997:ano2015)
   
 #Density by year
-p <- ggplot(alcaldias_long, aes(diff, colour = factor(year))) + geom_density()
+p <- ggplot(alcaldes_long, aes(diff, colour = factor(ano))) + geom_density()
 ggplotly(p)
 
 ###########################################################################################################
 ################################### LONGITUDINAL CLUSTER ANALYSIS #########################################
 ###########################################################################################################
 
-cld <- cld(alcaldias_wide, timeInData = c(6:11), idAll = alcaldias_wide$muni_code)
+cld <- cld(alcaldes_wide, timeInData = c(2:7), idAll = alcaldes_wide$codmpio)
 kml(cld, toPlot = "both")
 
 #Get clusters
-alcaldias_wide$cluster <- getClusters(cld, 6)
+alcaldes_wide$cluster <- getClusters(cld, 6)
 
 #Merge with geographical data
 setwd("~/Dropbox/Geografia/")
@@ -98,12 +109,12 @@ municipios_clust <- municipios %>%
   tidy(region = "MPIOS_1") %>%
   mutate(muni_code = as.numeric(id)) %>%
   merge(., municipios@data, by.x = "id", by.y = "MPIOS_1", all = T) %>%
-  merge(., alcaldias_wide, by.x = "id", by.y = "muni_code", all = T)
+  merge(., alcaldes_wide, by.x = "id", by.y = "codmpio", all = T)
 
 
 # Plot using ggplot
 setwd("~/Desktop/")
-g <- ggplot(data = municipios_clust, aes(x = long, y = lat)) + geom_polygon(aes(group = NOMBRE_MPI, fill = cluster))
+g <- ggplot(data = municipios_clust, aes(x = long, y = lat)) + geom_polygon(aes(group = id, fill = cluster))
 g <- g + coord_equal()
 # g <- g + theme(legend.position = c(.15, .25))
 ggplotly(g)
