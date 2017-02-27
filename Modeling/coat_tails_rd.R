3014e996b03a7d0774e25e86a2c3fb9ee06e9f24
leonardobonilla <leonardobonilla84@gmail.com>
2017-02-27 17:51:36 -0500

    RD

--- Modeling/coat_tails_rd.R
+++ Modeling/coat_tails_rd.R
@@ -14,7 +14,8 @@ setwd("D:/Users/lbonilme/Dropbox/CEER v2/Papers/Elecciones/")
 data <-"Data/CEDE/Microdatos/"
 res <-"Data/CEDE/Bases/"
 dnp <- "Data/DNP/Ejecuciones/"
-  
+invias <- "Data/invias/"
+
 ###########################################################################################################
 ######################################## ELECTIONS DATA ###################################################
 ###########################################################################################################
@@ -34,13 +35,13 @@ alcaldes_merge_r2 <- alcaldes_merge %>%
   filter(rank <= 2) %>% 
   merge(., coalitions, by.x = c("codpartido","ano") , by.y = c("party_code", "year_lag_presidencial"), all.x = T) %>%
 #   merge(., coalitions_con, by = c("codmpio", "codpartido","ano"), all.x = T) 
- arrange(codmpio, ano, codpartido) %>%
-  filter(is.na(coalition) == 0 & coalition != 98 & coalition != 99) %>% 
+  arrange(codmpio, ano, codpartido) %>%
+  filter(is.na(coalition) == 0 & coalition != 98 & coalition != 99) %>%
   mutate(ano = as.character(ano)) %>%
   group_by(codmpio, ano) %>%
-  mutate(n = 1, nn = sum(n)) %>% 
-  filter(nn==2) %>% 
-  dplyr::select(-c(codep,n,nn)) %>% 
+  mutate(n = 1, nn = sum(n)) %>%
+  filter(nn==2) %>%
+  dplyr::select(-c(codep,n,nn)) %>%
   merge(., controls[, c("pobl_tot", "coddepto", "ano", "codmpio")], by = c("codmpio", "ano"), all.x = T) 
 
 dim(alcaldes_merge_r2)
@@ -55,6 +56,7 @@ president <- readRDS(paste0(res, "presidentes_segunda_merge.rds")) %>%
 # Load ejecuciones
 ejecu_mean <- read_dta(paste0(dnp,"Ejecuciones_coat_mean.dta"))
 vias_mean <- read_dta(paste0(dnp,"Vias_SICEP_mean.dta"))
+invias_mean <- read_dta(paste0(invias,"invias_mean.dta"))
 ejecu_dep <- read_dta(paste0(dnp,"Ejecuciones_coat_dep.dta"))
 # ejecu_before1 <- read_dta(paste0(dnp,"Ejecuciones_coat_before1.dta"))
 # ejecu_after1 <- read_dta(paste0(dnp,"Ejecuciones_coat_after1.dta"))
@@ -77,57 +79,53 @@ alcaldes_rd <- alcaldes_merge_r2 %>%
   mutate(win_t = ifelse(rank == 1, 1, 0)) %>% 
   merge(., president,  by.x = c("year", "codmpio", "coalition"), by.y = c("ano", "codmpio", "coalition"), 
         suffixes = c("_t", "_t1"), all.x = T) %>%
-  merge(., ejecu_dep,  by.x = c("ano", "codmpio"), by.y = c("per", "codmpio"), all.x = T) %>%
-  mutate(eje_dep1 = ifelse(eje_dep >= 0.8, 1,0)) %>%
   merge(., ejecu_mean,  by.x = c("ano", "codmpio"), by.y = c("per", "codmpio"), all.x = T) %>%
   merge(., vias_mean,  by.x = c("ano", "codmpio"), by.y = c("per", "codmpio"), all.x = T) %>%
+  merge(., invias_mean,  by.x = c("ano", "codmpio"), by.y = c("per", "codmpio"), all.x = T) %>%
+  merge(., ejecu_dep,  by.x = c("ano", "codmpio"), by.y = c("per", "codmpio"), all.x = T) %>%
+  mutate(eje_dep1 = ifelse(eje_dep >= 0.8, 1,0)) %>%
   dplyr::select(codmpio, pobl_tot, coddepto, ano, year, codpartido_t, win_t, 
                 votos_t, votos_t1, starts_with("prop"), starts_with("eje_"), starts_with("vias"),starts_with("f_"), starts_with("log")) %>% 
   filter(is.na(prop_votes_total_t1)==0 & is.na(prop_votes_c2)==0) %>%
   arrange(codmpio, ano)
 
    
+############################
+# RD and OLS regressions 
 
 # Second rounds only
 l <- alcaldes_rd  %>% filter(ano != 2000 & ano != 2003) 
-dim(l)
-# hist(l$prop_votes_c2)
-
-# %>% filter(prop_votes_c2 <= 0.5 + sd(prop_votes_c2) * 1.96 & prop_votes_c2 >= 0.5 - sd(prop_votes_c2) * 1.96)
 l2 <- l %>% filter(prop_votes_c2 <= 0.6 & prop_votes_c2 >= 0.4)
 
-
-############################
-# RD and OLS regressions 
+# outcomes
+# out <- c("log_A1000","log_A1010","log_A1020","log_B","log_B1000","log_B1010","log_B1020","log_B1030","log_E1000","log_E2000","log_D1000", "log_D2000", "log_D3000", "prop_votes_total_t1")
+# out <- c("log_E1000","log_E2000","log_D1000", "log_D2000", "log_D3000")
+out <- c("vias","f_nac2","f_nac3","prop_votes_total_t1")
+# out <- c("prop_votes_total_t1")
 
 # Regressions for list of outcomes
 l_f <- function(o){
   r <- rdrobust(y = l[,o],
                 x = l$prop_votes_c2,
-                covs = cbind(as.factor(l$ano), l$pobl_tot, as.factor(l$coddepto)),
+                covs = cbind(l$pobl_tot, as.factor(l$coddepto)),
                 c = 0.5,
                 all = T,
-                vce = "nn")
+                vce = "hc1")
   rdplot(y=l2[,o], x=l2$prop_votes_c2, c = 0.5, 
-         binselect="es", nbins= 15, kernel="triangular", p=3, ci=95, 
+         binselect="es", nbins= 14, kernel="triangular", p=3, ci=95, 
   )
   return(r)
 }
 
-# out <- c("log_A1000","log_A1010","log_A1020","log_B","log_B1000","log_B1010","log_B1020","log_B1030","log_E1000","log_E2000","log_D1000", "log_D2000", "log_D3000", "prop_votes_total_t1")
-out <- c("log_vias","log_f_nac2","log_f_nac3","prop_votes_total_t1")
-
 lapply(out, l_f) 
 
 # linear
-
 lm_f <- function(o){
   r <- summary(lm(l[,o] ~ prop_votes_c2 + pobl_tot + as.factor(ano) + as.factor(coddepto), l))
   return(r)
 }
 
 lapply(out, lm_f) 
-
 
 
 ############################
@@ -190,27 +188,46 @@ rdplot(y=l2$prop_votes_total_t1, x=l2$prop_votes_c2, c = 0.5,
 )
 
 
-
-
 ############################
-# ggplot RD 
-g <- l %>%
-  mutate(bin = cut(prop_votes_c2, breaks = seq(0.3, 0.7, 0.001), include.lowest = T)) %>%
-  group_by(bin) %>%
-  summarize(mean_bin = mean(prop_votes_total_t1), sd_bin = sd(prop_votes_total_t1), n = length(codmpio)) %>%
-  .[complete.cases(.),] %>%
-  as.data.frame() %>%
-  mutate(treatment = ifelse(as.numeric(row.names(.)) >= 172, 1, 0), bins = row.names(.)) %>%
-  mutate(bins = mapvalues(.$bins, from = c(1:347), to = seq(0.329, 0.675, 0.001)))
+# RD and OLS regressions: Santos II (vias para la prosperidad)
 
-#RD Graph 
-p <- ggplot(g, aes(y = mean_bin, x = as.numeric(bins), colour = as.factor(treatment)))
-p <- p + geom_point(colour = "black", size = 1)
-p <- p + stat_smooth(data = alcaldes_rd, aes(x = prop_votes_c2, y = prop_votes_total_t1, 
-                                             colour = as.factor(win_t)), method = "loess", level = 0.9) 
-p <- p + scale_x_continuous(limits = c(0.45, 0.55))
-# p <- p + coord_cartesian(xlim = c(0.45, 0.55))
-p
+# Second rounds only
+l <- alcaldes_rd  %>% filter(ano == 2011) 
+l2 <- l %>% filter(prop_votes_c2 <= 0.6 & prop_votes_c2 >= 0.4)
+
+# out <- c("log_A1000","log_A1010","log_A1020","log_B","log_B1000","log_B1010","log_B1020","log_B1030","log_E1000","log_E2000","log_D1000", "log_D2000", "log_D3000", "prop_votes_total_t1")
+# out <- c("log_E1000","log_E2000","log_D1000", "log_D2000", "log_D3000")
+# out <- c("log_vias","log_f_nac2","log_f_nac3","prop_votes_total_t1")
+ out <- c("log_vias_ter","vias_ter","vias_ter_d","prop_votes_total_t1")
+
+# Regressions for list of outcomes
+l_f <- function(o){
+  r <- rdrobust(y = l[,o],
+                x = l$prop_votes_c2,
+                covs = cbind(l$pobl_tot, as.factor(l$coddepto)),
+                c = 0.5,
+                all = T,
+                vce = "hc1")
+  rdplot(y=l2[,o], x=l2$prop_votes_c2, c = 0.5, 
+         binselect="es", nbins= 15, kernel="triangular", p=3, ci=95, 
+  )
+  return(r)
+}
+
+lapply(out, l_f) 
+
+
+# linear
+lm_f <- function(o){
+  r <- summary(lm(l[,o] ~ prop_votes_c2 + pobl_tot +  as.factor(coddepto), l))
+  return(r)
+}
+
+lapply(out, lm_f) 
+
+
+
+
 
 
 
@@ -305,3 +322,30 @@ rdrobust(y = l$prop_votes_total_t1,
 
 
 
+
+
+
+###########################################################################################################
+##################################### RD: REVERSE COAT-TAILS EFFECT #######################################
+############################################ miscellaneous ################################################
+###########################################################################################################
+
+############################
+# ggplot RD 
+g <- l %>%
+  mutate(bin = cut(prop_votes_c2, breaks = seq(0.3, 0.7, 0.001), include.lowest = T)) %>%
+  group_by(bin) %>%
+  summarize(mean_bin = mean(prop_votes_total_t1), sd_bin = sd(prop_votes_total_t1), n = length(codmpio)) %>%
+  .[complete.cases(.),] %>%
+  as.data.frame() %>%
+  mutate(treatment = ifelse(as.numeric(row.names(.)) >= 172, 1, 0), bins = row.names(.)) %>%
+  mutate(bins = mapvalues(.$bins, from = c(1:347), to = seq(0.329, 0.675, 0.001)))
+
+#RD Graph 
+p <- ggplot(g, aes(y = mean_bin, x = as.numeric(bins), colour = as.factor(treatment)))
+p <- p + geom_point(colour = "black", size = 1)
+p <- p + stat_smooth(data = alcaldes_rd, aes(x = prop_votes_c2, y = prop_votes_total_t1, 
+                                             colour = as.factor(win_t)), method = "loess", level = 0.9) 
+p <- p + scale_x_continuous(limits = c(0.45, 0.55))
+# p <- p + coord_cartesian(xlim = c(0.45, 0.55))
+p
